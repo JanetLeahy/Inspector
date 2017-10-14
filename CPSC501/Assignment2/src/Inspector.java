523773899c82ca4cb3cf3589b94a7b6007524575
Janet Leahy <j.leahy886@gmail.com>
2017-10-14 10:46:21 -0600

    Initial commit

---
+++ CPSC501/Assignment2/src/Inspector.java
@@ -0,0 +1,16 @@
+
+public class Inspector {
+
+	/*
+	 * Inspects the object passed to find out its name, functions, 
+	 * constructors and fields, as well as those of any superclasses
+	 * and interfaces, and prints the results to standard output
+	 * 
+	 * @param	obj - the object to be inspected
+	 * @param	recursive - if true, fully inspects the fields of the passed object,
+	 * otherwise, simply returns the object's information
+	 */
+	public void inspect(Object obj, boolean recursive) {
+		System.out.println("This is a function");
+	}
+}
